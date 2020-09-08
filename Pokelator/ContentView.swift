8953f684dc83098891dcf70ba091c76c87f5487f
fredclausen <clausen.fred@icloud.com>
2020-09-08 16:38:58 -0600

    Initial Commit

---
+++ Pokelator/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Pokelator
+//
+//  Created by Frederick Clausen II on 9/8/20.
+//  Copyright © 2020 Frederick Clausen II. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
